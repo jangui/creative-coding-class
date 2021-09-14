d78c92518937f98900e57ee4873128ea6896e8af
jangui <jaimitino@gmail.com>
2021-09-14 16:50:11 -0400

    finished fundamentals

---
+++ fundamentals/task.js
@@ -0,0 +1,9 @@
+function listArtist(artistArr) {
+    artistArr.forEach(artist => {
+        console.log(artist);
+    })
+}
+
+
+artist = ['eminem', 'rhcp', 'mac miller', 'led zepplin']
+listArtist(artist);

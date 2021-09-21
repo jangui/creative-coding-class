1a7c913e7badf1b88c0b5f00ffb6a78852e89dcd
jangui <jaimitino@gmail.com>
2021-09-21 16:25:14 -0400

    bug fix on aninode sketch

--- sketches/aninodes.js
+++ sketches/aninodes.js
@@ -7,14 +7,15 @@ const settings = {
   animate: true
 };
 
-const gradient = ['#5c2af1', '#0052ff', '#006aff', '#007aff', '#0086ff', '#008fff', '#0096e9', '#009ccc', '#00a1ac', '#00a58c', '#00a96d', '#00ac51', '#11ad38']
+//const gradient = ['#5c2af1', '#0052ff', '#006aff', '#007aff', '#0086ff', '#008fff', '#0096e9', '#009ccc', '#00a1ac', '#00a58c', '#00a96d', '#00ac51', '#11ad38']
 
-//const gradient = ['#c7e84e', '#d0e142', '#d8da36', '#e0d32b', '#e7cb21', '#efc319', '#f6bb13', '#fcb211', '#ffaa13', '#ffa018', '#ff971f', '#ff8d27', '#ff832f', '#ff7837', '#ff6d3f', '#ff6148', '#ff5550', '#ff4759', '#ff3963', '#ff286c', '#ff1276', '#ff0080', '#ff008a', '#ff0094', '#ff009f', '#f800a9', '#ee00b4', '#e100be', '#d300c8', '#c317d1']
+const gradient = ['#c7e84e', '#d0e142', '#d8da36', '#e0d32b', '#e7cb21', '#efc319', '#f6bb13', '#fcb211', '#ffaa13', '#ffa018', '#ff971f', '#ff8d27', '#ff832f', '#ff7837', '#ff6d3f', '#ff6148', '#ff5550', '#ff4759', '#ff3963', '#ff286c', '#ff1276', '#ff0080', '#ff008a', '#ff0094', '#ff009f', '#f800a9', '#ee00b4', '#e100be', '#d300c8', '#c317d1']
 
 const sketch = ({context, width, height}) => {
   agents = [];
   totalAgents = 40;
 
+  // create agents and store in array
   for (let i = 0; i < totalAgents; i++) {
       const x = random.range(0, width);
       const y = random.range(0, height);
@@ -28,21 +29,19 @@ const sketch = ({context, width, height}) => {
     context.fillStyle = 'black';
     context.fillRect(0, 0, width, height);
 
+    // check distance to other agents
     for (let i = 0; i < agents.length; i++) {
         agent = agents[i];
-
         for (let j = i+1; j < agents.length; j++) {
             otherAgent = agents[j];
-
             dist = agent.pos.dist(otherAgent.pos);
-
             if (dist > 200) continue;
-
+            // if other agent close enough, store that agent & dist
             agent.connections.push([dist, otherAgent]);
         }
-
     }
 
+    // update and draw each agent
     agents.forEach(agent => {
         agent.update(width, height);
         agent.draw(context);
@@ -123,9 +122,14 @@ class Agent {
     }
 
     updateColor() {
-        let len = this.connections.length;
-        if (len > 8) len = 8;
-        let index = math.mapRange(this.connections.length, 0, 8, gradient.length-1, 0);
+        // max connections will effect the agents color
+        // if the agent has more connections than max
+        // then they will still be connected but the color wont change past max
+        let maxConnections = 12;
+        if (maxConnections > gradient.length-1) maxConnections = gradient.length-1;
+        let connections = this.connections.length;
+        if (connections > maxConnections) connections = maxConnections;
+        let index = math.mapRange(connections, 0, maxConnections, gradient.length-1, 0);
         this.color = gradient[Math.round(index)];
     }
 
